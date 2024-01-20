4884b9ae90ca5aa7df769cc5eccccb3e24023c50
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2024-01-21 01:04:34 +0545

    Learning

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Aboutpage = () => {
+  return (
+    <div className='text-3xl'>
+      <h1 className="">Hello Nischal this is about</h1>
+      <Link href="/" className='text-3xl'>Go to Home</Link>
+    </div>
+  )
+}
+
+export default Aboutpage
